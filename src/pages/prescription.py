1f7c45cdd7b54832661b9594ad37a9069988a6d6
prabodhw96 <prabodhwankhede1996@gmail.com>
2021-02-10 20:56:10 +0530

    add write permission

--- src/pages/prescription.py
+++ src/pages/prescription.py
@@ -23,7 +23,11 @@ def generate_sequence_for_change(changed_index, changed_value):
 	req_rand_vec = coef*random_vec
 	final_vec = np.insert(req_rand_vec,changed_index,changed_value)
 	d = pd.DataFrame(final_vec)
-	d.to_csv("src/data/init.csv", index=False)
+	try:
+		d.to_csv("src/data/init.csv", index=False)
+	except:
+		os.chmod("src/data/init.csv", 777)
+		d.to_csv("src/data/init.csv", index=False)
 	st.experimental_rerun()
 	#rerun.rerun()
 
